14fdd4fba60f1618fdce94bb51896ca27905b93b
Kaushal <157907997+KAUSHAL36977@users.noreply.github.com>
2025-01-23 11:50:00 +0530

    Update NEWWORDS.py

--- NEWWORDS.py
+++ NEWWORDS.py
@@ -7,3 +7,15 @@ nltk.download('words')
 word_list = words.words()
 print(len(word_list))  # Number of words
 print(word_list[:10])  # Sample words
+# Importing nltk and downloading the word corpus
+import nltk
+from nltk.corpus import words
+nltk.download('words')
+
+# Importing PyDictionary for word meanings, synonyms, and antonyms
+from PyDictionary import PyDictionary
+dictionary = PyDictionary()
+
+# Importing pyenchant for spell checking and dictionary lookups
+import enchant
+
